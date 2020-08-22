bd7180625bd7b5ea43280a86176e8a314c88a61b
iagosoares95 <iagosoaessantos@gmail.com>
2020-08-22 15:25:35 -0300

    Inserindo função de compactaçao das fotos

--- server.js
+++ server.js
@@ -1,44 +1,72 @@
 const express = require('express');
 const fs = require('fs');
 const request = require('request');
+const archiver = require('archiver');
+const async = require('async');
+
 const app = express();
 
-var count = 1;
+var count = 0;
+var zip_photos = 'images.zip';
+var photos_file = 'images.txt'
 
-const getPhotoFiles = (req, res) => {
-    const fs = require('fs');
-    let photo_dir = 'images/';
-    let photo = req.params.filename;
-    let final_path = photo_dir.concat(photo);
-    res.download(final_path);
-};
-
-var savePhotos = function(img) {
-    let name = 'images/photo_';
-    let photo_type = '.png';
-
-    if (!fs.existsSync('images')) {
-        fs.mkdirSync('images');
-    }
-
-    request(img).pipe(fs.createWriteStream(name.concat(count, photo_type)));
-    count++;
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
 
-var readImageFile = function() {
-    fs.readFile('images.txt', (err, data) => {
-        if (err) throw err;
-        let student = JSON.parse(data);
+var savePhotos = function(img) {
+    let name = 'photo_';
+    let photo_type = '.png';
+    count++;
+    request(img).pipe(fs.createWriteStream(name.concat(count, photo_type)));
+}
 
-        for (const img of student.images) {
+async function readImageFile(file) {
+    fs.readFile(file, (err, data) => {
+        if (err) throw err;
+        let photos = JSON.parse(data);
+
+        for (const img of photos.images) {
             savePhotos(img);
         }
     });
 }
 
-readImageFile();
+function compactPhotos(zip_file) {
+    var output = fs.createWriteStream(zip_file);
+    var archive = archiver('zip');
+
+    output.on('close', function() {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+    });
+
+    archive.on('error', function(err) {
+        throw err;
+    });
+
+    archive.pipe(output);
+
+    archive.glob('*.png');
+    archive.finalize();
+}
+
+const getPhotoFiles = (req, res) => {
+    const fs = require('fs');
+    let photo = req.params.filename;
+    res.download(photo);
+};
+
+async function main() {
+    readImageFile(photos_file);
+    await sleep(15000);
+    compactPhotos(zip_photos);
+}
 
 app.get('/', (req, res) => {
+    //compactPhotos(zip_photos);
     res.sendFile(__dirname + `/public/html/index.html`);
 });
 
@@ -46,12 +74,18 @@ app.get('/download_photo/:filename', getPhotoFiles);
 
 app.get('/get_photos', (req, res) => {
     let files_ = [];
-    var files = fs.readdirSync('images/');
-    res.send(files);
+    fs.readdirSync(__dirname).forEach(file => {
+        if (file.includes('.zip')) {
+            files_.push(file);
+        }
+    });
+    res.send(files_);
 });
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, () => {
+    //readImageFile(photos_file);
+    main();
     console.log('Listening on localhost:3000');
 });
